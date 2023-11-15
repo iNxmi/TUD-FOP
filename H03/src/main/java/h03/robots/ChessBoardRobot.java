4a0193f1f603f93bc7fd7594f4c7973e2d7866aa
Memphis <memphispc70@gmail.com>
2023-11-15 22:23:33 +0100

    Added ChatGPT auto generated JavaDoc to H03

--- H03/src/main/java/h03/robots/ChessBoardRobot.java
+++ H03/src/main/java/h03/robots/ChessBoardRobot.java
@@ -1,19 +1,3 @@
-//package h03.robots;
-//
-//import fopbot.RobotFamily;
-//
-//public class ChessBoardRobot extends MultiFamilyRobot {
-//
-//    public ChessBoardRobot(int x, int y, RobotFamily initialEven, RobotFamily initialOdd) {
-//        super(x, y, (x + y) % 2 == 0 ? new RobotFamily[]{initialEven, initialOdd} : new RobotFamily[]{initialOdd, initialEven});
-//    }
-//
-//    public ChessBoardRobot(int x, int y) {
-//        super(x, y, new RobotFamily[]{RobotFamily.SQUARE_BLACK, RobotFamily.SQUARE_WHITE});
-//    }
-//
-//}
-
 package h03.robots;
 
 import fopbot.RobotFamily;
--- H03/src/main/java/h03/robots/RGBRobot.java
+++ H03/src/main/java/h03/robots/RGBRobot.java
@@ -1,24 +1,3 @@
-//package h03.robots;
-//
-//import fopbot.RobotFamily;
-//
-//public class RGBRobot extends MultiFamilyRobot {
-//
-//    private static final RobotFamily[]
-//            NORMAL = new RobotFamily[]{RobotFamily.SQUARE_RED, RobotFamily.SQUARE_GREEN, RobotFamily.SQUARE_BLUE},
-//            INVERTED = new RobotFamily[]{RobotFamily.SQUARE_BLUE, RobotFamily.SQUARE_GREEN, RobotFamily.SQUARE_RED};
-//
-//    public RGBRobot(int x, int y, boolean inverted) {
-//        super(x, y, inverted ? INVERTED : NORMAL);
-//    }
-//
-//    public void testRGB() {
-//        for (int i = 0; i < 3; i++)
-//            exchange();
-//    }
-//
-//}
-
 package h03.robots;
 
 import fopbot.RobotFamily;
